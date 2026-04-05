55a55bf1e7fdf43d959c5b31e435ad02504ad4aa
krish dhiman <krishdhiman86@gmail.com>
2026-04-05 22:37:27 +0530

    constructor in inheritence

--- Inheritence/constructor.java
+++ Inheritence/constructor.java
@@ -37,9 +37,8 @@ public class constructor {
         //derived d = new derived();
         //derived d = new derived(2,2);
         //chlidOfDerivde cd = new chlidOfDerivde();
-        
+
         chlidOfDerivde d = new chlidOfDerivde(2, 3, 4);
         
     }
-    
 }

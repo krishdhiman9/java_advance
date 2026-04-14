71ad2631f91d6195f081e1e1a202eca2baa9e9cb
krish dhiman <krishdhiman86@gmail.com>
2026-04-14 21:45:47 +0530

    practice question

--- Inheritence/circle_cylinder.java
+++ Inheritence/circle_cylinder.java
@@ -1,15 +1,20 @@
 package Inheritence;
 class circle{
     int r = 3;
-    int  = 3.14;
-    circle(int r, int ){
-     System.out.println();
-    } 
+    
+    circle(){
+     System.out.println("area of circle " + 3.14*r*r);
+    }
+    circle(int r){
+        
+    }
+
+
 
 }
 
 public class circle_cylinder {
     public static void main(String[] args) {
-        
+        circle c = new circle();
     }
 }

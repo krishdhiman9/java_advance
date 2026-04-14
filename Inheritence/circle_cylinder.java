b0e8f6dd77de8b3c65a5bb574f693e7a0453952a
krish dhiman <krishdhiman86@gmail.com>
2026-04-14 21:31:15 +0530

    prac. question

---
+++ Inheritence/circle_cylinder.java
@@ -0,0 +1,15 @@
+package Inheritence;
+class circle{
+    int r = 3;
+    int  = 3.14;
+    circle(int r, int ){
+     System.out.println();
+    } 
+
+}
+
+public class circle_cylinder {
+    public static void main(String[] args) {
+        
+    }
+}

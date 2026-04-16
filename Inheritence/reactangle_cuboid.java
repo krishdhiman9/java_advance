f2664123b7654eb1cb09cfb1b519a728eaeaa0cb
krish dhiman <krishdhiman86@gmail.com>
2026-04-16 22:11:45 +0530

    practice q

--- Inheritence/reactangle_cuboid.java
+++ Inheritence/reactangle_cuboid.java
@@ -1,14 +1,26 @@
 package Inheritence;
 class rectangle{
     rectangle(){
-        
+        System.out.println("i am in rectangle");
+    }
+    rectangle(int x){
+        System.out.println("i am in super key word 1");
+    }
+}
+class cuboid{
+    cuboid(){
+        System.out.println("i am in cuboid ");
+    }
+    cuboid(int x, int y ){
+        super(x);
+        System.out.println("i am in super key word 2");
     }
 }
 
 
 public class reactangle_cuboid {
     public static void main(String[] args) {
-        
+        cuboid c = new cuboid()
 
     }
 }

3c384de2bf0623cfb4f46d8a88c7b5dc14bf021d
krish dhiman <krishdhiman86@gmail.com>
2026-05-01 21:02:49 +0530

    practice question.

--- Recursion/test.java
+++ Recursion/test.java
@@ -1,4 +1,10 @@
 public class test{
-	public static void main(String[]args){
-		System.out.println("hello");}}
-
+	public static void main(String[] args) {
+		int [][] arr = {{1,2,3},{4,5,6}};
+		for(int i=0; i<arr.length; i++){
+			for(int j=0; j<arr[i].length; j++){
+				System.out.print(arr[i][j] + " ");
+			}System.out.println();
+		}
+	} 
+}

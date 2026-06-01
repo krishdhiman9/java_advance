f17c1cac7a7d880904a697d509a49febed9c2eaf
krish dhiman <krishdhiman86@gmail.com>
2026-06-01 22:25:52 +0530

    practice question

---
+++ Inheritence/name.java
@@ -0,0 +1,33 @@
+package Inheritence;
+class namee{
+    int age ;
+    String name;
+    double marks;
+
+    namee(){
+        age = 0;
+        name = "Unknown";
+        marks = 0;
+    }
+
+    namee(int age){
+        System.out.println(this.age=age);
+        this.name="unknown";
+        this.marks=0;
+    }
+    namee(int age,double marks){
+        this.age=age;
+        this.name="null";
+        this.marks=marks;
+    }
+}
+public class name {
+    public static void main(String[] args) {
+        namee n = new namee();
+        System.out.println(n.age);
+        System.out.println(n.name);
+        System.out.println(n.marks);
+
+
+    }
+}

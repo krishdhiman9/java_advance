c291fa2b2491b7f4744efa055c98dcbf35b9bdf0
krish dhiman <krishdhiman86@gmail.com>
2026-06-02 20:41:44 +0530

    constructor overloading

--- Inheritence/name.java
+++ Inheritence/name.java
@@ -22,7 +22,7 @@ class namee{
 }
 public class name {
     public static void main(String[] args) {
-        namee n = new namee();
+        namee n = new namee(23,23.4);
         System.out.println(n.age);
         System.out.println(n.name);
         System.out.println(n.marks);

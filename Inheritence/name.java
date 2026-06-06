1e570af560017149a91c4c6b39032a7d6c430b19
krish dhiman <krishdhiman86@gmail.com>
2026-06-06 20:58:14 +0530

    prac. question

--- Inheritence/name.java
+++ Inheritence/name.java
@@ -14,15 +14,15 @@ class namee{
         this.name="unknown";
         this.marks=0;
     }
-    namee(int age,double marks) {
+    namee(int age,String name,double marks) {
         this.age = age;
-        this.name = "null";
+        this.name = name;
         this.marks = marks;
     }
 }
 public class name {
     public static void main(String[] args) {
-        namee n = new namee(24,23.4);
+        namee n = new namee(24,"krish", 3.4);
         System.out.println(n.age);
         System.out.println(n.name);
         System.out.println(n.marks);

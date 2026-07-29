7321372958ad67e888830962913e8a2fc1e3944d
krish dhiman <krishdhiman86@gmail.com>
2026-07-29 21:08:15 +0530

    use setter getter

---
+++ encapsulation/revision.java
@@ -0,0 +1,22 @@
+
+package encapsulation;
+class student{
+    private  int roll = 0 ;
+
+    void setRoll(int r){
+
+        roll = r;
+    }
+    int getRoll(){
+
+        return roll;
+    }
+}
+public class revision  {
+    static void main() {
+        student n = new student();
+        n.setRoll(56);
+        System.out.println(n.getRoll());
+    }
+}
+
